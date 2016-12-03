29a2bdd2751cd2fed26963f54614528dc3caa29d
Ahmad Magdy <ahmedmagdi@outlook.com>
2016-12-03 14:59:31 +0200

    Update Files

--- Program.cs
+++ Program.cs
@@ -1,104 +1,90 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.WebSockets;
-using System.Text;
-using System.Threading.Tasks;
-using MoreLinq;
-
-
-namespace CPUSchedulingAlgorithms
+﻿
+class Program
 {
-
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<SJFModel> m1 = new List<SJFModel>();
+        string pn;
+        int bu, at;
+        while (true)
         {
-            List<SJFModel> m1 = new List<SJFModel>();
-            string pn;
-            int bu, at;
-            while (true)
-            {
 
-                Console.Write("Enter the name of The Process: ");
-                pn = Console.ReadLine();
-                if (pn == string.Empty) break;
-                Console.Write("Enter the Arrive Time: ");
-                at = int.Parse(Console.ReadLine());
-                Console.Write("Enter the Burst Time: ");
-                bu = int.Parse(Console.ReadLine());
-                m1.Add(new SJFModel(pn,  bu, at));
-            }
-
-            SchedulingAlgorithms.SJFPreemptive(m1);
-            List<HashModel> h = new List<HashModel>
-            {
-                new HashModel("P1", 12),
-                new HashModel("P2", 8),
-                new HashModel("P3", 4),
-                new HashModel("P4", 10),
-                new HashModel("P5", 5)
-                //new HashModel("P1", 10),
-                //new HashModel("P2", 1),
-                //new HashModel("P3", 2),
-                //new HashModel("P4", 1),
-                //new HashModel("P5", 5)
-                //new HashModel("P1", 6),
-                //new HashModel("P2", 5),
-                //new HashModel("P3", 2),
-                //new HashModel("P4", 3),
-                //new HashModel("P5", 7)
-            };
-            SchedulingAlgorithms.RoundRubin(h, 5);
-            List<SJFModel> m2 = new List<SJFModel>()
-            {
-				new SJFModel("P1", 7, 0),
-                new SJFModel("P2", 4, 2),
-                new SJFModel("P3", 1, 4),
-                new SJFModel("P4", 4, 5)
-
-            };
-            SchedulingAlgorithms.SJFPreemptive(m2);
-            List<SJFModel> m = new List<SJFModel>()
-            {
-				new SJFModel("P1", 8, 0),
-                new SJFModel("P2", 4, 1),
-                new SJFModel("P3", 9, 2),
-                new SJFModel("P4", 5, 3)
-
-            };
-            SchedulingAlgorithms.SJFPreemptive(m);
-			
-			List<SJFModel> m = new List<SJFModel>()
-            {
-                new SJFModel("P1",7,0),
-                new SJFModel("P2",4,2),
-                new SJFModel("P3",1,4),
-                new SJFModel("P4",4,5),
-            };
-            SchedulingAlgorithms.SJFNonPreemptive(m);
-			
-			List<SJFModel> m = new List<SJFModel>
-            {
-				new SJFModel("P1",3,0),
-                new SJFModel("P2",2,1),
-                new SJFModel("P3",1,2)
-
-            };
-            SchedulingAlgorithms.RoundRubinAT(m,2);
-			
-			List<PriorityModel> p = new List<PriorityModel>()
-            {
-                new PriorityModel("P1",10,3),
-                new PriorityModel("P2",1,1),
-                new PriorityModel("P3",2,4),
-                new PriorityModel("P4",1,5),
-                new PriorityModel("P5",5,2)
-			};
-            SchedulingAlgorithms.Priority(p);
-
+            Console.Write("Enter the name of The Process: ");
+            pn = Console.ReadLine();
+            if (pn == string.Empty) break;
+            Console.Write("Enter the Arrive Time: ");
+            at = int.Parse(Console.ReadLine());
+            Console.Write("Enter the Burst Time: ");
+            bu = int.Parse(Console.ReadLine());
+            m1.Add(new SJFModel(pn, bu, at));
         }
-    }
 
+        SchedulingAlgorithms.SJFPreemptive(m1);
+        List<HashModel> h = new List<HashModel>
+        {
+            new HashModel("P1", 12),
+            new HashModel("P2", 8),
+            new HashModel("P3", 4),
+            new HashModel("P4", 10),
+            new HashModel("P5", 5)
+            //new HashModel("P1", 10),
+            //new HashModel("P2", 1),
+            //new HashModel("P3", 2),
+            //new HashModel("P4", 1),
+            //new HashModel("P5", 5)
+            //new HashModel("P1", 6),
+            //new HashModel("P2", 5),
+            //new HashModel("P3", 2),
+            //new HashModel("P4", 3),
+            //new HashModel("P5", 7)
+        };
+        SchedulingAlgorithms.RoundRubin(h, 5);
+        List<SJFModel> m2 = new List<SJFModel>()
+        {
+            new SJFModel("P1", 7, 0),
+            new SJFModel("P2", 4, 2),
+            new SJFModel("P3", 1, 4),
+            new SJFModel("P4", 4, 5)
+
+        };
+        SchedulingAlgorithms.SJFPreemptive(m2);
+        List<SJFModel> m = new List<SJFModel>()
+        {
+            new SJFModel("P1", 8, 0),
+            new SJFModel("P2", 4, 1),
+            new SJFModel("P3", 9, 2),
+            new SJFModel("P4", 5, 3)
+
+        };
+        SchedulingAlgorithms.SJFPreemptive(m);
+
+        List<SJFModel> m = new List<SJFModel>()
+        {
+            new SJFModel("P1", 7, 0),
+            new SJFModel("P2", 4, 2),
+            new SJFModel("P3", 1, 4),
+            new SJFModel("P4", 4, 5),
+        };
+        SchedulingAlgorithms.SJFNonPreemptive(m);
+
+        List<SJFModel> m = new List<SJFModel>
+        {
+            new SJFModel("P1", 3, 0),
+            new SJFModel("P2", 2, 1),
+            new SJFModel("P3", 1, 2)
+
+        };
+        SchedulingAlgorithms.RoundRubinAT(m, 2);
+
+        List<PriorityModel> p = new List<PriorityModel>()
+        {
+            new PriorityModel("P1", 10, 3),
+            new PriorityModel("P2", 1, 1),
+            new PriorityModel("P3", 2, 4),
+            new PriorityModel("P4", 1, 5),
+            new PriorityModel("P5", 5, 2)
+        };
+        SchedulingAlgorithms.Priority(p);
+
+    }
 }
--- SchedulingAlgorithms.cs
+++ SchedulingAlgorithms.cs
@@ -1,261 +1,253 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CPUSchedulingAlgorithms
+﻿
+public class SJFModel
 {
-    public class SJFModel
+    public SJFModel(string processName, int BurstTime, int ArriveTime = 0)
     {
-        public SJFModel(string processName, int BurstTime, int ArriveTime=0)
-        {
-            this.ProcessName = processName;
-            this.ArriveTime = ArriveTime;
-            this.BurstTime = BurstTime;
-        }
-
-        public string ProcessName { get; set; }
-        public int ArriveTime { get; set; }
-        public int BurstTime { get; set; }
-        public int WT { get; set; } = 0;
-        public int LastEnd { get; set; } = 0;
+        this.ProcessName = processName;
+        this.ArriveTime = ArriveTime;
+        this.BurstTime = BurstTime;
     }
 
-    public class HashModel
-    {
-        public HashModel(string name, int value)
-        {
-            this.Name = name;
-            this.Value = value;
-        }
-        public string Name { get; set; }
-        public int Value { get; set; }
-        public int WT { get; set; } = 0;
-        public int LastEnd { get; set; } = 0;
-    }
-    
-    public class FCFSModel
-    {
-        public FCFSModel(string Name,int AT,int BT)
-        {
-            this.Name = Name;
-            this.AT = AT;
-            this.BT = BT;
-        }
+    public string ProcessName { get; set; }
+    public int ArriveTime { get; set; }
+    public int BurstTime { get; set; }
+    public int WT { get; set; } = 0;
+    public int LastEnd { get; set; } = 0;
+}
 
-        public string Name { get; set; }
-        public int AT { get; set; }
-        public int BT { get; set; }
-        public int WT { get; set; }
-    }
-	
-	public class PriorityModel
+public class HashModel
+{
+    public HashModel(string name, int value)
     {
-        public PriorityModel(string name, int burstTime,int priority)
-        {
-            this.Name = name;
-            this.BurstTime = burstTime;
-            this.Priority = priority;
-        }
-        public string Name { get; set; }
-        public int BurstTime { get; set; }
-        public int Priority { get; set; }
-        public int WT { get; set; } = 0;
+        this.Name = name;
+        this.Value = value;
+    }
+    public string Name { get; set; }
+    public int Value { get; set; }
+    public int WT { get; set; } = 0;
+    public int LastEnd { get; set; } = 0;
+}
 
+public class FCFSModel
+{
+    public FCFSModel(string Name, int AT, int BT)
+    {
+        this.Name = Name;
+        this.AT = AT;
+        this.BT = BT;
     }
 
-    public class SchedulingAlgorithms
+    public string Name { get; set; }
+    public int AT { get; set; }
+    public int BT { get; set; }
+    public int WT { get; set; }
+}
+
+public class PriorityModel
+{
+    public PriorityModel(string name, int burstTime, int priority)
     {
-        public static void SJFPreemptive(IList<SJFModel> sjfModels)
-        {
-            string tempElementName = string.Empty;
-            SJFModel tempElement;
-            for (int i = 0;; i++)
-            {
+        this.Name = name;
+        this.BurstTime = burstTime;
+        this.Priority = priority;
+    }
+    public string Name { get; set; }
+    public int BurstTime { get; set; }
+    public int Priority { get; set; }
+    public int WT { get; set; } = 0;
 
-                tempElement =
-                    sjfModels.Where(e => e.ArriveTime <= i && e.BurstTime > 0)
-                        .OrderBy(e => e.BurstTime)
-                        .FirstOrDefault();
-                if (tempElement == null)
+}
+
+public class SchedulingAlgorithms
+{
+    public static void SJFPreemptive(IList<SJFModel> sjfModels)
+    {
+        string tempElementName = string.Empty;
+        SJFModel tempElement;
+        for (int i = 0; ; i++)
+        {
+
+            tempElement =
+                sjfModels.Where(e => e.ArriveTime <= i && e.BurstTime > 0)
+                    .OrderBy(e => e.BurstTime)
+                    .FirstOrDefault();
+            if (tempElement == null)
+            {
+                Console.WriteLine($"|{0}|\t {i}");
+                break;
+            }
+
+            tempElement.BurstTime -= 1;
+            //if (item.ArriveTime < counter) continue;
+            if (tempElementName == tempElement.ProcessName) continue;
+
+            if (i != 0)
+            {
+                Console.WriteLine(
+                    $"|{sjfModels.FirstOrDefault(x => x.ProcessName == tempElementName).BurstTime}|\t {i}");
+                sjfModels.FirstOrDefault(x => x.ProcessName == tempElementName).LastEnd = i;
+            }
+
+            Console.Write($"{i}\t({tempElement.ProcessName})");
+
+            tempElementName = tempElement.ProcessName;
+
+            tempElement.WT = tempElement.LastEnd == 0
+                ? i - tempElement.ArriveTime
+                : i - tempElement.LastEnd;
+        }
+        int sum = 0;
+        sum = sjfModels.Sum(x => x.WT);
+        //foreach (var item in sjfModels)
+        //{
+        //    sum += item.WT;
+        //}
+        Console.WriteLine($"AWT is = {(double)sum / sjfModels.Count} ");
+    }
+
+    public static void SJFNonPreemptive(IList<SJFModel> sjfModels)
+    {
+        SJFModel holder;
+        int counter = 0;
+        while (true)
+        {
+            holder = sjfModels.Where(i => i.ArriveTime <= counter && i.BurstTime > 0)
+                .OrderBy(x => x.BurstTime).FirstOrDefault();
+            if (holder == null) break;
+            holder.WT = counter - holder.ArriveTime;
+            Console.Write($"{counter}\t {holder.ProcessName} ({holder.BurstTime})\t ");
+            counter += holder.BurstTime;
+            holder.BurstTime -= holder.BurstTime;
+            Console.WriteLine(counter);
+        }
+
+        int sum = 0;
+        sum = sjfModels.Sum(x => x.WT);
+        Console.WriteLine($"AWT is = {(double)sum / sjfModels.Count} ");
+
+    }
+
+
+    public static void RoundRubin(List<HashModel> testHashModels, int quantum)
+    {
+        int tempbegin = 0;
+        string highest = "";
+        bool outx = false;
+        highest = testHashModels.FirstOrDefault(x => x.Value == testHashModels.Max(p => p.Value)).Name;
+
+        for (;;)
+        {
+            foreach (var item in testHashModels)
+            {
+                if (item.Value < 1)
+                    continue;
+                Console.Write(tempbegin + "\t");
+                Console.Write($"{item.Name} ({item.Value})");
+                item.WT = tempbegin - item.LastEnd + item.WT;
+                if (item.Value - quantum < 0)
                 {
-                    Console.WriteLine($"|{0}|\t {i}");
-                    break;
+                    tempbegin += item.Value;
+                    item.Value -= item.Value;
                 }
 
-                tempElement.BurstTime -= 1;
-                //if (item.ArriveTime < counter) continue;
-                if (tempElementName == tempElement.ProcessName) continue;
-
-                if (i != 0)
+                else
                 {
-                    Console.WriteLine(
-                        $"|{sjfModels.FirstOrDefault(x => x.ProcessName == tempElementName).BurstTime}|\t {i}");
-                    sjfModels.FirstOrDefault(x => x.ProcessName == tempElementName).LastEnd = i;
+                    item.Value -= quantum;
+                    tempbegin += quantum;
                 }
 
-                Console.Write($"{i}\t({tempElement.ProcessName})");
-
-                tempElementName = tempElement.ProcessName;
-
-                tempElement.WT = tempElement.LastEnd == 0
-                    ? i - tempElement.ArriveTime
-                    : i - tempElement.LastEnd;
+                Console.Write($"||{item.Value}\t");
+                Console.WriteLine(tempbegin);
+                if (item.Name == highest && item.Value < 1) outx = true;
+                item.LastEnd = tempbegin;
             }
-            int sum = 0;
-            sum = sjfModels.Sum(x => x.WT);
-            //foreach (var item in sjfModels)
-            //{
-            //    sum += item.WT;
-            //}
-            Console.WriteLine($"AWT is = {(double) sum/sjfModels.Count} ");
-        }
-		
-		public static void SJFNonPreemptive(IList<SJFModel> sjfModels)
-        {
-            SJFModel holder;
-            int counter = 0;
-            while (true)
-            {
-                holder = sjfModels.Where(i => i.ArriveTime <= counter && i.BurstTime >0)
-                    .OrderBy(x => x.BurstTime).FirstOrDefault();
-                if(holder == null) break;
-                holder.WT = counter - holder.ArriveTime;
-                Console.Write($"{counter}\t {holder.ProcessName} ({holder.BurstTime})\t ");
-                counter += holder.BurstTime;
-                holder.BurstTime -= holder.BurstTime;
-                Console.WriteLine(counter);
-            }
-
-            int sum = 0;
-            sum = sjfModels.Sum(x => x.WT);
-            Console.WriteLine($"AWT is = {(double)sum / sjfModels.Count} ");
+            if (outx) break;
 
         }
-		
+        testHashModels.ForEach(x => Console.WriteLine(x.WT));
+        int sum = testHashModels.Sum(x => x.WT);
+        Console.WriteLine($"AWT  = {sum / testHashModels.Count}");
+    }
 
-        public static void RoundRubin(List<HashModel> testHashModels, int quantum)
+    //RoundRubin With Arrive Time
+    public static void RoundRubinAT(List<SJFModel> models, int quantum)
+    {
+        models = models.OrderBy(x => x.ArriveTime).ToList();
+        int tempbegin = 0;
+        string tempname = "";
+        bool outx = false;
+        var temList = models.Where(x => x.ArriveTime <= tempbegin && x.BurstTime > 0).ToList();
+        for (;;)
         {
-            int tempbegin = 0;
-            string highest = "";
-            bool outx = false;
-            highest = testHashModels.FirstOrDefault(x => x.Value == testHashModels.Max(p => p.Value)).Name;
 
-            for (;;)
+            foreach (var item in temList)
             {
-                foreach (var item in testHashModels)
+                if (item.ProcessName.Equals(tempname) && temList.Count > 1)
+                    continue;
+                Console.Write(tempbegin + "\t");
+                Console.Write($"{item.ProcessName} ({item.BurstTime})");
+                item.WT = tempbegin - item.LastEnd - item.ArriveTime + item.WT;
+                item.ArriveTime = 0;
+                if (item.BurstTime - quantum < 0)
                 {
-                    if (item.Value < 1)
-                        continue;
-                    Console.Write(tempbegin + "\t");
-                    Console.Write($"{item.Name} ({item.Value})");
-                    item.WT = tempbegin - item.LastEnd + item.WT;
-                    if (item.Value - quantum < 0)
-                    {
-                        tempbegin += item.Value;
-                        item.Value -= item.Value;
-                    }
-
-                    else
-                    {
-                        item.Value -= quantum;
-                        tempbegin += quantum;
-                    }
-
-                    Console.Write($"||{item.Value}\t");
-                    Console.WriteLine(tempbegin);
-                    if (item.Name == highest && item.Value < 1) outx = true;
-                    item.LastEnd = tempbegin;
+                    tempbegin += item.BurstTime;
+                    item.BurstTime -= item.BurstTime;
                 }
-                if (outx) break;
 
-            }
-            testHashModels.ForEach(x => Console.WriteLine(x.WT));
-            int sum = testHashModels.Sum(x => x.WT);
-            Console.WriteLine($"AWT  = {sum/testHashModels.Count}");
-        }
-		
-		 //RoundRubin With Arrive Time
-        public static void RoundRubinAT(List<SJFModel> models, int quantum)
-        {
-			models = models.OrderBy(x => x.ArriveTime).ToList();
-            int tempbegin = 0;
-            string tempname = "";
-            bool outx = false;
-            var temList  = models.Where(x => x.ArriveTime <= tempbegin && x.BurstTime > 0).ToList(); 
-            for (;;)
-            {
-                
-                foreach (var item in temList)
+                else
                 {
-                    if (item.ProcessName.Equals(tempname) && temList.Count > 1)
-                        continue;
-                    Console.Write(tempbegin + "\t");
-                    Console.Write($"{item.ProcessName} ({item.BurstTime})");
-                    item.WT = tempbegin - item.LastEnd - item.ArriveTime + item.WT;
-                    item.ArriveTime = 0;
-                    if (item.BurstTime - quantum < 0)
-                    {
-                        tempbegin += item.BurstTime;
-                        item.BurstTime -= item.BurstTime;
-                    }
-
-                    else
-                    {
-                        item.BurstTime -= quantum;
-                        tempbegin += quantum;
-                    }
-
-                    Console.Write($"||{item.BurstTime}\t");
-                    Console.WriteLine(tempbegin);
-                    
-                    item.LastEnd = tempbegin;
-                    tempname = item.ProcessName;
-                    temList = models.Where(x => x.ArriveTime <= tempbegin && x.BurstTime > 0).OrderBy(x=> x.LastEnd).ToList();
-                    if (temList.Count <1) outx = true;
+                    item.BurstTime -= quantum;
+                    tempbegin += quantum;
                 }
-                if (outx) break;
 
+                Console.Write($"||{item.BurstTime}\t");
+                Console.WriteLine(tempbegin);
+
+                item.LastEnd = tempbegin;
+                tempname = item.ProcessName;
+                temList =
+                    models.Where(x => x.ArriveTime <= tempbegin && x.BurstTime > 0).OrderBy(x => x.LastEnd).ToList();
+                if (temList.Count < 1) outx = true;
             }
-            models.ForEach(x => Console.WriteLine($"{x.ProcessName}:  {x.WT}"));
-            int sum = models.Sum(x => x.WT);
-            Console.WriteLine($"AWT  = {sum / models.Count}");
-        }
-		
-        
-         public static void FCFS(ICollection<FCFSModel> models)
-        {
-            models = models.OrderBy(p => p.AT).ToList();
-            int counter = 0;
-            foreach (var item in models)
-            {
-                item.WT = counter;
-                Console.Write($"{counter}\t {item.Name}({item.BT})\t");
-                counter += item.BT;
-                Console.WriteLine(counter);
-            }
-            int sum = models.Sum(x => x.WT);
-            Console.WriteLine($"AWT  = {sum / models.Count}");
-        }
-		
-		public static void Priority(IList<PriorityModel> models)
-        {
-            models = models.OrderBy(i => i.Priority).ToList();
-            int counter = 0;
-            foreach (var item in models)
-            {
-                Console.Write($"{counter}\t |{item.Name}({item.Priority})|\t");
-                item.WT = counter;
-                counter += item.BurstTime;
-                item.BurstTime -= item.BurstTime;
-                Console.WriteLine(counter);
-            }
-            int sum = models.Sum(x => x.WT);
-            Console.WriteLine($"AWT is = {(double) sum/models.Count} ");
+            if (outx) break;
 
         }
-        
+        models.ForEach(x => Console.WriteLine($"{x.ProcessName}:  {x.WT}"));
+        int sum = models.Sum(x => x.WT);
+        Console.WriteLine($"AWT  = {sum / models.Count}");
+    }
+
+
+    public static void FCFS(ICollection<FCFSModel> models)
+    {
+        models = models.OrderBy(p => p.AT).ToList();
+        int counter = 0;
+        foreach (var item in models)
+        {
+            item.WT = counter;
+            Console.Write($"{counter}\t {item.Name}({item.BT})\t");
+            counter += item.BT;
+            Console.WriteLine(counter);
+        }
+        int sum = models.Sum(x => x.WT);
+        Console.WriteLine($"AWT  = {sum / models.Count}");
+    }
+
+    public static void Priority(IList<PriorityModel> models)
+    {
+        models = models.OrderBy(i => i.Priority).ToList();
+        int counter = 0;
+        foreach (var item in models)
+        {
+            Console.Write($"{counter}\t |{item.Name}({item.Priority})|\t");
+            item.WT = counter;
+            counter += item.BurstTime;
+            item.BurstTime -= item.BurstTime;
+            Console.WriteLine(counter);
+        }
+        int sum = models.Sum(x => x.WT);
+        Console.WriteLine($"AWT is = {(double)sum / models.Count} ");
+
     }
 
 }
